b3011c8fd7c66142a0d04dcd1c92216ce6f08635
Muhammad Usman Nazir <nazirusman721@gmail.com>
2026-01-03 14:44:34 +0500

    Day 3  Completed!

--- Arrays_and_Hashing/Two_sum.py
+++ Arrays_and_Hashing/Two_sum.py
@@ -2,10 +2,10 @@
 # Day 3: Arrays and Hashing
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}  
+        see = {}  
 
         for i, num in enumerate(nums):
             need = target - num
-            if need in seen:
-                return [seen[need], i]
-            seen[num] = i
+            if need in see:
+                return [see[need], i]
+            see[num] = i
